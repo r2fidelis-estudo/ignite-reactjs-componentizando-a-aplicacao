f384d837f8f5c3e9e7a08307b8429c09369d9fd5
Rubens Fidelis <r2fidelis@r2fidelis.com>
2021-03-26 22:24:34 -0300

    Desafio concluído

--- src/@types/custom.d.ts
+++ src/@types/custom.d.ts
@@ -1,1 +1,18 @@
 declare module '*.svg'
+
+export interface GenreResponseProps {
+  id: number
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family'
+  title: string
+}
+
+export interface MovieProps {
+  imdbID: string
+  Title: string
+  Poster: string
+  Ratings: Array<{
+    Source: string
+    Value: string
+  }>
+  Runtime: string
+}
